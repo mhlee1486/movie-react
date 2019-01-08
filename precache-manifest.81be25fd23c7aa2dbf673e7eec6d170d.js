a0bc80b25ab1c54fcbad7c7e0ea77398ba9433fb
Sosimsimgo <mhlee1486@gmail.com>
2019-01-08 14:47:14 +0900

    Updates

---
+++ precache-manifest.81be25fd23c7aa2dbf673e7eec6d170d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d48dd0be5e274dc52010",
+    "url": "/movie-react/static/js/runtime~main.d48dd0be.js"
+  },
+  {
+    "revision": "0379ed939c65d8a6aed4",
+    "url": "/movie-react/static/js/main.0379ed93.chunk.js"
+  },
+  {
+    "revision": "1754c8ed087440f5abea",
+    "url": "/movie-react/static/js/1.1754c8ed.chunk.js"
+  },
+  {
+    "revision": "0379ed939c65d8a6aed4",
+    "url": "/movie-react/static/css/main.ec0c79cc.chunk.css"
+  },
+  {
+    "revision": "b19ee32c38bf9d74e018d7b40e909a67",
+    "url": "/movie-react/index.html"
+  }
+];
